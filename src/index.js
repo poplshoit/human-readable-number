3e38a032a4fe0df9dc870d70e897a2dcc3fb17c7
Maksim Popushoi <poplshoy@gmail.com>
2020-10-19 21:27:11 +0300

    feat: end human read num task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (number) { 
+    if (number < 100) {
+        return twoDigit(number);
+    } else if (number % 100 === 0) {
+        return numbers[number / 100] + ' hundred';
+    } else {
+        return threeDigit(number);
+    }
+}
+
+let numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+                    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+function twoDigit(num) {
+    if (num < 20) {
+        return numbers[num];
+    } else if (num % 10 === 0){
+        return tens[(num / 10) - 2];
+    } else {
+        return tens[Math.trunc(num / 100 * 10) - 2]+ ' ' + numbers[num % 10];
+    }
+}
+
+function threeDigit(num) {
+    return numbers[Math.trunc(num / 100)] + ' hundred ' + twoDigit(num % 100);
 }
